acd30af590bd386edd3b872af637d4561743c5e7
Muckki <ckdduq0508@gmail.com>
2025-02-19 20:49:00 +0900

    25.02.19 기능 정리 및 수정, 도구 박스만 생성성

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let pageState = 0;
 let pageTiming = 1000;
 function changePages(el){
-    if (pageState == 0) {
+    if (pageState == 0 && !$(`div[name=${$(el).attr('name')}]`).hasClass("active")) {
         pageState = 1;
         $(".pages").removeClass("active");
         setTimeout(() => {
             $(".pages").addClass("d-none");
-            $(`[name=${$(el).attr('name')}`).removeClass("d-none").addClass("active");
+            $(`div[name=${$(el).attr('name')}]`).removeClass("d-none").addClass("active");
             setTimeout(() => {
                 pageState = 0;
             }, pageTiming);
@@ -16,4 +16,15 @@ function changePages(el){
     }
 }
 
-// pageTiming 변경하는 함수 추가 예정( 도구 툴 )
+let toolState = 0;
+function toolClick(el){
+    if (toolState == 0) {
+        toolState = 1;
+        $(el).toggleClass("active");
+        setTimeout(() => {
+            $(el).removeClass("active");
+            toolState = 0;
+        }, 700);
+    }
+}
+// pageTiming 변경하는 함수 추가 예정( 도구 툴 ) , 로컬스토리지에 저장해 관리 추가
